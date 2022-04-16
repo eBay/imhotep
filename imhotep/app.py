76d8934e2371ef0029528f79de7a229129c02128
Mingyang Li <tslmy@outlook.com>
2022-04-16 10:32:23 -0700

    pos_map[0] is only used when report_file_violation

--- imhotep/app.py
+++ imhotep/app.py
@@ -189,11 +189,10 @@ class Imhotep:
                 pos_map: Dict[int, int] = {
                     x.number: x.position for x in entry.added_lines
                 }
-                pos_map[0] = min(pos_map.values())
-
                 added_lines: List[int] = [l.number for l in entry.added_lines]
                 if self.report_file_violations:
                     # "magic" value of line 0 represents file-level results.
+                    pos_map[0] = min(pos_map.values())
                     added_lines.append(0)
 
                 violations: Dict[str, List[str]] = results.get(
